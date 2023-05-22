0a891563bf5854e027e04348c9a1c72cf808a949
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-05-22 12:39:38 +0300

    task1-comments

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -8,8 +8,9 @@ import { ProductModel } from '../../products/models/product.model';
 })
 
 export class CartService {
-  
+
   getCartItems() {
+    // обычно у модели записи корзины есть доп поля, например, количество
     const items: ProductModel[] = [{id: 2, name: 'STAR BRITE Power Trim & Tilt Fluid', description: 'Machine oil', price: 500, category: Category.Automotive, isAvailable: false}];
     return items;
   }
